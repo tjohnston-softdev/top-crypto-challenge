74173c5474b625f3f32c1c14cacbfa923b8def46
Tyrone Johnston <tyrone.johnston@hotmail.com>
2021-09-10 15:42:46 +1000

    Defined 'GET '/api/coins/top' endpoint

--- changelog.md
+++ changelog.md
@@ -1,4 +1,9 @@
 # Changelog
 
-**./routes/coin-status.js**
-* File deleted.
+**./routes/coins.js**
+* Corrected 'Coin**G**ecko' capitalization for 'status' comment.
+* Changed outer bracket structure for `/status`
+* Removed blank line before 'coingecko.checkStatus'
+* Defined `/top` endpoint.
+	* Retrieves top 100 coins by market value.
+	* If there is an error, the message will be sent as a reply.
--- routes/coins.js
+++ routes/coins.js
@@ -2,14 +2,32 @@ const express = require('express');
 const router = express.Router();
 const coingecko = require("../src/coingecko");
 
-// GET Coingecko API status.
-router.get('/status', function(req, res, next) {
-  
+// GET CoinGecko API status.
+router.get('/status', function(req, res, next)
+{
   coingecko.checkStatus(function (statusMsg)
   {
 	  res.send(statusMsg);
   });
   
 });
+
+
+// GET top 100 coins by market cap.
+router.get('/top', function(req, res, next)
+{
+	coingecko.getToplist(function (coinsErr, coinsRes)
+	{
+		if (coinsErr !== null)
+		{
+			res.send(coinsErr.message);
+		}
+		else
+		{
+			res.send(coinsRes);
+		}
+	});
+});
+
 
 module.exports = router;
