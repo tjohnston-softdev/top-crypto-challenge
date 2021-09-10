dada2ecbf970665bff34cd13db8589d16b13017a
Tyrone Johnston <tyrone.johnston@hotmail.com>
2021-09-10 14:37:44 +1000

    Local origin HTTP test
    Added 'Status' button to home page.
    Retrieves CoinGecko API status from localhost.

---
+++ public/scripts/local-req.js
@@ -0,0 +1,18 @@
+function callStatusRequest()
+{
+	var outputElement = document.getElementById("txtStatus");
+	var requestObj = new XMLHttpRequest();
+	
+	outputElement.innerHTML = "Checking . . .";
+	
+	requestObj.onreadystatechange = function()
+	{
+		if (this.readyState === 4 && this.status === 200)
+		{
+			outputElement.innerHTML = requestObj.responseText;
+		}
+	};
+	
+	requestObj.open("GET", "api/coin-status");
+	requestObj.send();
+}
